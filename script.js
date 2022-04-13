821402c1edf4555767c41601cf04901be7564d14
Chris Soncrant <christopher.soncrant@gmail.com>
2022-04-13 13:18:21 -0700

    hamburger menu created

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburgerMenu = document.getElementById('hamburger-icon');
+const hamburgerLinks = document.getElementById('hamburger-links');
+
+hamburgerLinks.style.display = "none";
+
+function showHideMenu() {
+    if (hamburgerLinks.style.display === 'none' || hamburgerLinks.style.display === '') {
+        hamburgerLinks.style.display = 'block';
+
+    } else {
+        hamburgerLinks.style.display = 'none';
+    };
+}
+
+hamburgerMenu.addEventListener('click', showHideMenu)
